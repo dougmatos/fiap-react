a95664a055d1ea2b99a2eb1e76a29b9040d40ec9
dougmatos <dougmooca@gmail.com>
2019-06-09 22:02:12 -0300

    Updates

---
+++ precache-manifest.77457b4670dfaa7cfeeabebb5e82e53d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc50f7dd769377b6c9ba1ac7620d6663",
+    "url": "/fiap-react/index.html"
+  },
+  {
+    "revision": "b77e5fafaa7845d10c8d",
+    "url": "/fiap-react/static/css/main.96ff3cfc.chunk.css"
+  },
+  {
+    "revision": "6bde24c05b57ee5195b3",
+    "url": "/fiap-react/static/js/2.889199ce.chunk.js"
+  },
+  {
+    "revision": "b77e5fafaa7845d10c8d",
+    "url": "/fiap-react/static/js/main.58c1a8c7.chunk.js"
+  },
+  {
+    "revision": "76f3dd4e9f4f5da98ae8",
+    "url": "/fiap-react/static/js/runtime~main.4f4220b6.js"
+  }
+]);
